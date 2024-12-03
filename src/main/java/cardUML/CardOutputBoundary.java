3c8202c08ad0356dc6df06527fb974a36605e73d
Jokemaker-afk <jackye.zheng@mail.utoronto.ca>
2024-12-03 00:21:52 -0500

    1

--- src/main/java/cardUML/CardOutputBoundary.java
+++
@@ -1,9 +0,0 @@
-package cardUML;
-
-public interface CardOutputBoundary {
-    /**
-     * Used to renew the GUI as a help function to add/delete card.
-     * @param cardOutput is the card list
-     */
-    void refresh(CardOutput cardOutput);
-}
